57d2a2c7b49806b2e437cdda2bc85a6163555d13
vito <ssca321@outlook.com>
2024-01-06 17:00:51 +0800

    更新mosdns

--- docker-conf/data/mosdns/entrypoint.sh
+++ docker-conf/data/mosdns/entrypoint.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 set -e
 
-if [ -z "$(apk list --installed curl)" ]
-then
+if [ -z "$(apk list --installed curl)" ]; then
     sed -i 's/dl-cdn.alpinelinux.org/mirrors.tuna.tsinghua.edu.cn/g' /etc/apk/repositories
-    apk update && apk add --no-cache curl
+    apk update && apk add --no-cache curl tzdata
 fi
 
 cat <<EOF | crontab -
 0 0 * * * sh /etc/mosdns/update_v2dat.sh > /dev/stdout
 EOF
 
-if [ ! -f /etc/mosdns/geoip.dat ]; then
+if [ ! -f /etc/mosdns/geosite_category-ads-all.txt ]; then
     sh /etc/mosdns/update_v2dat.sh
 fi
 
-crond && mosdns start --dir /etc/mosdns
+crond && mosdns start -d /etc/mosdns
--- docker-conf/data/mosdns5/entrypoint.sh
+++ docker-conf/data/mosdns5/entrypoint.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 set -e
 
-if [ -z "$(apk list --installed curl)" ]
-then
+if [ -z "$(apk list --installed curl)" ]; then
     sed -i 's/dl-cdn.alpinelinux.org/mirrors.ustc.edu.cn/g' /etc/apk/repositories
-    apk update && apk add --no-cache curl
+    apk update && apk add --no-cache curl tzdata
 fi
 
 cat <<EOF | crontab -
@@ -15,4 +14,4 @@ if [ ! -f /etc/mosdns/site_proxy.txt ]; then
     sh /etc/mosdns/update_v2dat.sh
 fi
 
-crond && mosdns start --dir /etc/mosdns
+crond && mosdns start -d /etc/mosdns
