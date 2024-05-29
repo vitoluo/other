73103101e802d3133b51f2fe5523eaf4269c7fcf
vito <ssca321@outlook.com>
2024-05-29 17:27:00 +0800

    update

--- QuantumultX/gen_ad.snippet.py
+++ QuantumultX/gen_ad.snippet.py
@@ -1,18 +1,12 @@
 import os
-
-
-def read_from_line(file_path, start_line):
-    with open(file_path, "r", encoding="utf-8") as file:
-        for _ in range(start_line - 1):
-            next(file)
-        for line in file:
-            yield line.strip()
+import time
+import requests
 
 
 if __name__ == "__main__":
-    sourceFile = "D:\download\chongxie.txt"
+    sourceUrl = "https://github.com/fmz200/wool_scripts/raw/main/QuantumultX/rewrite/chongxie.txt"
     targetFile = "ad.snippet"
-    needAds = [
+    needAds = {
         "12306",
         "百度贴吧",
         "哔哩哔哩",
@@ -49,16 +43,7 @@ if __name__ == "__main__":
         "中国银行",
         "中国银行 缤纷生活",
         "中国电信",
-    ]
-
-    adNameDict = {}
-    hostnameSet = set()
-
-    with open(sourceFile, "r", encoding="utf-8") as f:
-        for num, line in enumerate(f, 1):
-            if line.startswith("# > "):
-                name = line.replace("# > ", "").replace("\n", "")
-                adNameDict[name] = num
+    }
 
     if os.path.exists(targetFile):
         os.remove(targetFile)
@@ -75,28 +60,31 @@ if __name__ == "__main__":
             "#!raw-url=https://github.com/vitoluo/other/raw/master/QuantumultX/ad.snippet"
         )
         f.write("\n")
-        f.write(
-            "#!remark=原始规则地址：https://github.com/fmz200/wool_scripts/raw/main/QuantumultX/rewrite/chongxie.txt"
-        )
+        f.write("#!date=" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+        f.write("\n")
+        f.write("#!remark=原始规则地址：" + sourceUrl)
         f.write("\n")
         f.write("\n")
 
-        for adName in needAds:
-            lineNum = adNameDict[adName]
-            if lineNum is not None:
-                count = 0
-                for line in read_from_line(sourceFile, lineNum):
-                    if count > 0 and line == "":
-                        break
-                    if count > 0 and line.startswith("# >"):
-                        break
+        resp = requests.get(sourceUrl, stream=True)
+        matchAd = False
+        if resp.status_code == 200:
+            for l in resp.iter_lines():
+                line = l.decode("utf-8")
+                if line.startswith("hostname ="):
                     f.write(line)
                     f.write("\n")
-                    if line.startswith("# hostname"):
-                        for hostname in line.replace("# hostname =", "").split(","):
-                            hostnameSet.add(hostname.strip())
-                    count += 1
-                f.write("\n")
-        f.write("hostname = ")
-        hostnames = ",".join(hostnameSet)
-        f.write(hostnames)
+                    f.write("\n")
+                elif line.startswith("# >"):
+                    name = line.replace("# > ", "")
+                    if name in needAds:
+                        matchAd = True
+                        f.write(line)
+                        f.write("\n")
+                    else:
+                        matchAd = False
+                elif matchAd:
+                    f.write(line)
+                    f.write("\n")
+        else:
+            print("文件更新失败")
