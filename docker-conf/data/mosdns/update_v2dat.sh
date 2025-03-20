002c053d590fee49343926815b5b4e38139c8ded
vito <ssca321@outlook.com>
2025-03-20 15:41:45 +0800

    update mosdns config

--- docker-conf/data/mosdns/update_v2dat.sh
+++ docker-conf/data/mosdns/update_v2dat.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-curl -L https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geoip.dat -o /etc/mosdns/geoip.dat
-curl -L https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geosite.dat -o /etc/mosdns/geosite.dat
+curl -L https://testingcf.jsdelivr.net/gh/MetaCubeX/meta-rules-dat@release/geoip.dat -o /etc/mosdns/geoip.dat
+curl -L https://testingcf.jsdelivr.net/gh/MetaCubeX/meta-rules-dat@release/geosite.dat -o /etc/mosdns/geosite.dat
 mosdns v2dat unpack-ip -o /etc/mosdns /etc/mosdns/geoip.dat:private,cn
 mosdns v2dat unpack-domain -o /etc/mosdns /etc/mosdns/geosite.dat:private,category-ads-all,cn,gfw
