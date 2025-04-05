602f126f8ae131d8bcb4b59cb6c4a218d81dc6f1
vito <ssca321@outlook.com>
2025-04-05 20:18:21 +0800

    update

--- docker-conf/data/mosdns/update_v2dat.sh
+++ docker-conf/data/mosdns/update_v2dat.sh
@@ -3,5 +3,5 @@ set -e
 
 curl -L https://testingcf.jsdelivr.net/gh/MetaCubeX/meta-rules-dat@release/geoip.dat -o /etc/mosdns/geoip.dat
 curl -L https://testingcf.jsdelivr.net/gh/MetaCubeX/meta-rules-dat@release/geosite.dat -o /etc/mosdns/geosite.dat
-mosdns v2dat unpack-ip -o /etc/mosdns /etc/mosdns/geoip.dat:private,cn
+mosdns v2dat unpack-ip -o /etc/mosdns /etc/mosdns/geoip.dat:private,cn,cloudflare
 mosdns v2dat unpack-domain -o /etc/mosdns /etc/mosdns/geosite.dat:private,category-ads-all,cn,gfw
