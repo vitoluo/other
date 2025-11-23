7362c800c9fd7d2d2d631228e3e56b81cf6f70b1
vito <ssca321@outlook.com>
2025-11-23 19:43:24 +0800

    添加 akamai 规则

--- docker-conf/data/mosdns/update_v2dat.sh
+++ docker-conf/data/mosdns/update_v2dat.sh
@@ -5,3 +5,8 @@ curl -L https://testingcf.jsdelivr.net/gh/MetaCubeX/meta-rules-dat@release/geoip
 curl -L https://testingcf.jsdelivr.net/gh/MetaCubeX/meta-rules-dat@release/geosite.dat -o /etc/mosdns/geosite.dat
 mosdns v2dat unpack-ip -o /etc/mosdns /etc/mosdns/geoip.dat:private,cn,cloudflare
 mosdns v2dat unpack-domain -o /etc/mosdns /etc/mosdns/geosite.dat:private,category-ads-all,cn,gfw
+
+curl -L https://testingcf.jsdelivr.net/gh/platformbuilds/Akamai-ASN-and-IPs-List@master/akamai_ip_list.lst -o /etc/mosdns/tmp_akamai_ip4
+curl -L https://testingcf.jsdelivr.net/gh/platformbuilds/Akamai-ASN-and-IPs-List@master/akamai_ipv6_list.lst -o /etc/mosdns/tmp_akamai_ip6
+cat tmp_akamai_ip4 tmp_akamai_ip6 > ip_akamai.txt
+rm -f tmp_akamai_ip4 tmp_akamai_ip6
